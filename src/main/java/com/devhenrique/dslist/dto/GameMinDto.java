09a38902f2a058b965ab2791088f3c1a6cffbf54
HenriqueCoqueiro <henriquecoqueiromelo@gmail.com>
2025-05-24 09:44:35 -0300

    Deploy ready

--- src/main/java/com/devhenrique/dslist/dto/GameMinDto.java
+++ src/main/java/com/devhenrique/dslist/dto/GameMinDto.java
@@ -6,6 +6,7 @@ import com.devhenrique.dslist.projections.GameMinProjection;
 public class GameMinDto {
     private long id;
     private String title;
+    private Integer year;
     private String imgUrl;
     private String shortDescription;
 
@@ -15,6 +16,7 @@ public class GameMinDto {
     public GameMinDto(Game entity) {
         this.id = entity.getId();
         this.title = entity.getTitle();
+        this.year = entity.getYear();
         this.imgUrl = entity.getImgUrl();
         this.shortDescription = entity.getShortDescription();
     }
@@ -32,6 +34,30 @@ public class GameMinDto {
 
     public String getTitle() {
         return title;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public Integer getYear() {
+        return year;
+    }
+
+    public void setYear(Integer year) {
+        this.year = year;
+    }
+
+    public void setImgUrl(String imgUrl) {
+        this.imgUrl = imgUrl;
+    }
+
+    public void setShortDescription(String shortDescription) {
+        this.shortDescription = shortDescription;
     }
 
     public String getImgUrl() {
